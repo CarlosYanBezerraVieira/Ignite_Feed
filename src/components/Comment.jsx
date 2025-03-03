e7fd5d78babbc26d1e27248fae7219637a938a41
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-03 20:40:26 -0300

    fix: applying good state update practice

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,7 +11,7 @@ export function Coment({ content, onDeleteComment }) {
     }
 
     function handleLikeComment() {
-        setLikeCount(likeCount + 1)
+        setLikeCount((state)=>{return state + 1})
     }
 
 
