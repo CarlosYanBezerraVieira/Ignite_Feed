0239013ed2b37eaa780ef24919316161f1d32320
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-03 18:01:47 -0300

    feat: using mocos data to vary post content and formatting the dates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,52 @@ import { Sidebar } from "./components/Sidebar"
 import styles from "./App.module.css"
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://www.github.com/CarlosYanBezerraVieira.png",
+      name: "Carlos Yan",
+      role: "Desenvolvedor Full Stack",
+    },
+    content: [
+      {
+        type: 'paragraph', content: "Hoje estou trabalhando com React na Rocketseat"
+      },
+      {
+        type: "paragraph", content: "Estou aprendendo a criar aplicações web com React"
+      },
+      {
+        type: "link", content: "https://rocketseat.com.br"
+      }
+
+    ],
+    publishedAt: new Date("2025-03-02T12:00:00"),
+
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://www.github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO Rocketseat",
+    },
+    content: [
+      {
+        type: 'paragraph', content: "Hoje estou ensinando sobre React na Rocketseat"
+      },
+      {
+        type: "paragraph", content: "Estou ensinando a criar aplicações web com React"
+      },
+      {
+        type: "link", content: "https://rocketseat.com.br"
+      }
+
+    ],
+    publishedAt: new Date("2025-03-03T12:00:00"),
+
+  }
+]
 
 export function App() {
 
@@ -13,14 +59,19 @@ export function App() {
     <>
       <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
 
         <main>
-          <Post author = "Carlos" content ="Tecnolgoia" />
-          <Post author = "Igor" content ="Comida" />
-          <Post author = "João" content ="Esportes" />
-          <Post author = "Maria" content ="Cinema" />
-      
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+           },)}
+
         </main>
 
 
